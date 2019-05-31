1dfb8aac880ea45a6a6a8e437959058d7f61f0c1
JOJO <a129273382@msn.com>
2019-05-31 17:38:29 +0800

    No4

---
+++ DemoMVC/Controllers/MovieRoomController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class MovieRoomController : Controller
+    {
+        private DataClassesDataContext DC = new DataClassesDataContext();
+        // GET: MovieRoom
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MovieRoom_List()
+        {
+            ViewBag.Title = "Hi";
+            var Rs = DC.MovieRoom_Row.OrderBy(q => q.SortNo);
+            return View(Rs);
+        }
+
+
+    }
+}
