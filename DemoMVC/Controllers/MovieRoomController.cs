a9bac0819e571a01fa5a315bec84209781bdb259
JOJO <a129273382@msn.com>
2019-06-02 01:33:06 +0800

    +MovieAdd

--- DemoMVC/Controllers/MovieRoomController.cs
+++ DemoMVC/Controllers/MovieRoomController.cs
@@ -14,7 +14,62 @@ namespace DemoMVC.Controllers
         {
             return View();
         }
+        public ActionResult MovieRoom_R_List()
+        {
+            var Rs = DC.MovieRoom_Row.OrderBy(q => q.SortNo);
+            return View(Rs);
+        }
+        public ActionResult MovieRoom_R_Remove(long ID)
+        {
+            var R = DC.MovieRoom_Row.FirstOrDefault(q => q.MRRID == ID);
+            if(R!=null)
+            {
+                var Cs = DC.MovieRoom_Cell.Where(q => q.MRRID == R.MRRID);
+                var Ss = from q in Cs
+                         join p in DC.NovieRoom_Sell
+                         on q.MRCID equals p.MRCID
+                         select p;
 
+                DC.NovieRoom_Sell.DeleteAllOnSubmit(Ss);
+                DC.SubmitChanges();
+                DC.MovieRoom_Cell.DeleteAllOnSubmit(Cs);
+                DC.SubmitChanges();
+                DC.MovieRoom_Row.DeleteOnSubmit(R);
+                DC.SubmitChanges();
+            }
+            MovieRoom_R_List();
+            return View("MovieRoom_R_List");
+        }
+
+        public ActionResult MovieRoom_R_Edit(long ID)
+        {
+            var R = DC.MovieRoom_Row.FirstOrDefault(q => q.MRRID == ID);
+            if(R!= null)
+            {
+                R = new MovieRoom_Row();
+            }
+            return View(R);
+        }
+        [HttpPost]
+        public ActionResult MovieRoom_R_Edit(long MRRID,string RowName,int SortNo)
+        {
+            var R = DC.MovieRoom_Row.FirstOrDefault(q => q.MRRID == MRRID);
+            if (R != null)
+            
+                R = new MovieRoom_Row();
+
+            R.RowName = RowName;
+            R.SortNo = SortNo;
+            R.UpdDate = DateTime.Now;
+            if(MRRID == 0)
+            {
+                R.CreDate = R.UpdDate;
+                DC.MovieRoom_Row.InsertOnSubmit(R);
+            }
+            DC.SubmitChanges();
+
+            return View("MovieRoom_R_List");
+        }
         public ActionResult MovieRoom_List()
         {
             var Rs = DC.MovieRoom_Row.OrderBy(q => q.SortNo);
